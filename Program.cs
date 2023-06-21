3ef8b676cabffee6e1a0558524899c055db015d2
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-06-21 15:07:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = { 1, 2, 5, 8, 12, 21, 4 };
+
+for (int j = 0; j < arr.Length; j++)
+{
+    for (int i = 0; i + 1 < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            var temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+
+}
+
+
+foreach (var item in arr)
+    Console.Write($"{item}, ");
+
